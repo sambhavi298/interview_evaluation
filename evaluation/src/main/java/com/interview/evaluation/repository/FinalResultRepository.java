44b3263f8c5a8bb426d4a907d81cb62bd2a1a8ab
sambhavi298 <sr3468@srmist.edu.in>
2026-03-07 11:27:37 +0530

    Add FinalResultRepository interface

---
+++ evaluation/src/main/java/com/interview/evaluation/repository/FinalResultRepository.java
@@ -0,0 +1,8 @@
+package com.interview.evaluation.repository;
+
+import com.interview.evaluation.model.FinalResult;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FinalResultRepository extends JpaRepository<FinalResult, Integer> {
+
+}
